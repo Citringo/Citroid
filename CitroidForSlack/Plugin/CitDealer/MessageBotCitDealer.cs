5ad634ad4e4a632c0874140083990f16964183fa
Citrine <citringo@gmail.com>
2017-02-23 23:38:45 +0900

    CitDealerが寂しいのでミニゲームの追加

--- CitroidForSlack/Plugin/CitDealer/MessageBotCitDealer.cs
+++ CitroidForSlack/Plugin/CitDealer/MessageBotCitDealer.cs
@@ -1,4 +1,5 @@
 ﻿using CitroidForSlack.Api;
+using CitroidForSlack.Extensions;
 using System;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -27,8 +28,9 @@ namespace CitroidForSlack.Plugins.CitDealer
 		{
 			//TODO: ゲームの状態をシリアライズさせる
 		}
-
 		
+		int count = 0;
+
 		public async Task InitializeAsync(ICitroid citroid)
 		{
 			
@@ -37,7 +39,47 @@ namespace CitroidForSlack.Plugins.CitDealer
 
 		public async Task RunAsync(Message mes, ICitroid citroid)
 		{
-			
+			if (mes.subtype != null)
+				return;
+
+			if (!mes.text.Contains("しとげーむ"))
+				return;
+
+			switch (count)
+			{
+				case 0:
+					await citroid.PostAsync(mes.channel, "... ゲームはまだできてないんだ。");
+					break;
+				case 1:
+					await citroid.PostAsync(mes.channel, "... まだできてないんだってば。");
+					break;
+				case 2:
+					await citroid.PostAsync(mes.channel, "しょうがないなあ、そんなに言うならじゃんけんでもするかい？");
+					break;
+				default:
+					var a = await citroid.PostAsync(mes.channel, "じゃあいくよ。 じゃんけん");
+					await a.AddReactionAsync("fist");
+					await a.AddReactionAsync("v");
+					await a.AddReactionAsync("hand");
+					string Judge(string myhand, string win, string lose) => myhand == win ? "僕の勝ち！" : myhand == lose ? "君の勝ち！" : "あいこ！" ;
+					a.ReactionAdded += async (emoji, user) =>
+					{
+						var te = new[] { "fist", "v", "hand" }.Random();
+						var output = "";
+						if (emoji == "fist")
+							output = Judge(te, "hand", "v");
+						else if (emoji == "v")
+							output = Judge(te, "fist", "hand");
+						else if (emoji == "hand")
+							output = Judge(te, "v", "fist");
+						else
+							output = "なんだその手は！";
+						a = await a.UpdateAsync($"君:{emoji}: :{te}:僕 {output}");
+					};
+
+					break;
+			}
+			count++;
 		}
 
 		#region どうでもいいやつ
